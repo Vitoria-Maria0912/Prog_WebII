ab8e8ec6a543926cebcd267f0eb52b3263f2fbe7
121210254 VITORIA MARIA DO NASCIMENTO <vitoria.nascimento@lcc2-03.lcc.ufcg.edu.br>
2024-07-10 12:04:14 -0300

    build: Configurando o ambiente.

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 const port = 3000;
+const path = require('path');
 
 app.get('/', (req, res) => {
     res.send('Hello World!');
@@ -8,4 +9,6 @@ app.get('/', (req, res) => {
 
 app.listen(port, () => {
     console.log(`Server running at http:localhost:${port}/`);
-})
+});
+
+app.use(express.static(path.join(__dirname, 'public')));
