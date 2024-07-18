94b673357eb3cd1c0fbe28b869986f63426b2fdb
121210254 VITORIA MARIA DO NASCIMENTO <vitoria.nascimento@lcc1-03.lcc.ufcg.edu.br>
2024-07-18 13:16:23 -0300

    build: Desenvolvimento de um Servidor RESTful com Node.js, Express e TypeScript.

--- server.js
+++ server.js
@@ -1,14 +1,15 @@
-const express = require('express');
-const app = express();
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const express_1 = __importDefault(require("express"));
+const app = (0, express_1.default)();
 const port = 3000;
-const path = require('path');
-
+app.use(express_1.default.json());
 app.get('/', (req, res) => {
-    res.send('Hello World!');
+    res.send('Hello, world!');
 });
-
 app.listen(port, () => {
-    console.log(`Server running at http:localhost:${port}/`);
+    console.log(`Servidor rodando em http://localhost:${port}`);
 });
-
-app.use(express.static(path.join(__dirname, 'public')));
