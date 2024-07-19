aadac2a528bbd5d959fc698013b0b38514a34220
Vitória Maria do Nascimento <116987726+Vitoria-Maria0912@users.noreply.github.com>
2024-07-19 16:56:13 -0300

    feat: Adicionando métodos post and getByID

--- server.js
+++ server.js
@@ -12,3 +12,26 @@ app.listen(port, () => {
 });
 
 app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/filmes?id=', (req, res) => {
+  const id = 1;
+  const filmes: FilmeInterface[] = JSON.parse(path.join(__dirname, '../database', 'filmes.json'));
+  const result = filmes.find(filme => filme.id === id);
+  if (result != undefined) {
+    res.sendFile(JSON.stringify(result));
+  } else {
+    res.status(404).send('Filme não encontrado');
+  }
+});
+
+app.post('/filmes', (req, res) => {
+  const dadosRecebidos = req.body;
+
+  try {
+    const jsonParsed = JSON.parse(JSON.stringify(dadosRecebidos));
+    
+    res.status(200).json({ mensagem: 'JSON válido', dados: jsonParsed });
+  } catch (error) {
+      res.status(400);
+  }
+});
